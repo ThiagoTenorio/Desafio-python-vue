270d47a4f28c9134fbeabcddce2a01b0c8eb39d8
ThiagoTenorio <thiago_tenorio43@hotmail.com>
2024-10-03 01:24:37 -0300

    Configurando banco e criando model

--- extration/data/models.py
+++ extration/data/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class CSVData(models.Model):
+    SUBSTANCIA = models.CharField(max_length=500)
+    CNPJ = models.CharField(max_length=500)
+    LABORATORIO = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return f"{self.SUBSTANCIA}, {self.CNPJ}, {self.LABORATORIO}"
